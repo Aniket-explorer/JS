272e252b0c557002828c44d643d3345b813a50aa
Aniket-explorer <kraniket5765@gmail.com>
2025-05-21 02:47:15 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS as new version
+
+// alert( 3 + 3)   // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3);  // code readability should be high
+
+console.log("Aniket");
+
+
+let name = "Aniket"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fasle 
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// obeject
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
+
+
+     
+
+     
