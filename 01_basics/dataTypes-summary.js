26542a50460846c9f9245211a6af7b00085a85ad
Aniket-explorer <kraniket5765@gmail.com>
2025-05-22 00:39:46 +0530

    dataType Summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigIngt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 12345678901234567890n
+
+
+
+// Reference (Non-Primitive)
+
+// Array, Objexts, Functions
+
+const heros = ["shaktiam", "naagraj", "doga"];
+let myObj = {
+    name: "aniket",
+    age: 23,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof outsideTemp);
