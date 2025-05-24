671fc13493e584f143b955910e45d65c00ac1391
Aniket-explorer <kraniket5765@gmail.com>
2025-05-25 05:07:59 +0530

    Object Part-2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,13 +48,40 @@ const users = [
 
 // console.log(users[1].email);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+const course = {
+    coursename: "JavaScript",
+    price: "999",
+    courseInstructor: "aniket"
+}
+
+// console.log(course.courseInstructor);
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name" : "aniket",
+//     "coursename" : "JavaScript",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
 
 
 
